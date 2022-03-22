12ab4ba7d96c3f5ee00485724f98fb6f012b8b89
kthss01 <kthss01@naver.com>
2022-03-23 07:52:18 +0900

    알고리즘 공부 52일차

--- Java/Baekjoon/Gold/Prob5_14500/Main.java
+++ Java/Baekjoon/Gold/Prob5_14500/Main.java
@@ -45,86 +45,113 @@ import java.util.StringTokenizer;
  * 
  * 출력
  *  최댓값 출력
+ * 
+ * -> 오랜시간 풀지 못해서 풀이보고 진행 DFS로 푸는거라고 함
  */
 
-class Point {
-    int x;
-    int y;
-    Point(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-}
-
-class Shape {
-    List<Point> position;
-
-    Shape(List<Point> position) {
-        this.position = position;
-    }
-
-    public int compute(int[][] board, int w, int h, int x, int y) {
-        int sum = 0;
-
-        for (Point point : position) {
-            int px = point.x + x;
-            int py = point.y + y;
-
-            if (px < 0 || px >= w || py < 0 || py >= h) {
-                return -1;
-            }
-
-            sum += board[px][py];
-        }
-
-        return sum;
-    }
-}
-
 public class Main {
+
+    private static int n, m, a[][], result;
+    private static boolean check[][];
+    private static int dx[] = {0, 0, 1, -1};
+    private static int dy[] = {-1, 1, 0, 0};
+
+    private static int ex[][] = {
+        {0,0,0,1}, {0,1,2,1}, {0,0,0,-1}, {0,-1,0,1}
+    };
+    private static int ey[][] = {
+        {0,1,2,1}, {0,0,0,1}, {0,1,2,1}, {0,1,1,1}
+    };
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer token = new StringTokenizer(br.readLine());
+        StringTokenizer st;
 
-        // Shape 만들기
-        List<Shape> shapes = makeShape();
+        // input
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
 
-        int n = Integer.parseInt(token.nextToken());
-        int m = Integer.parseInt(token.nextToken());
-
-        int[][] board = new int[n][m];
-
-        for (int i = 0; i < n; i++) {
-            token = new StringTokenizer(br.readLine());
-            for (int j = 0; j < m; j++) {
-                board[i][j] = Integer.parseInt(token.nextToken());
+        a = new int[n+1][m+1];
+        check = new boolean[n+1][m+1];
+        
+        for (int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= m; j++) {
+                a[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
+        // compute
+        // 2차원 배열 각각의 원소에서 검사 수행
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                // DFS
+                
+                // 방문했던 점 또 방문해야 함
+                // 들어가기전 체크 후 끝났을 때 체크 해제
+                check[i][j] = true;
 
+                dfs(i, j, a[i][j], 1);
+
+                // 길이로 재귀를 중단시키기 때문에
+                // 수행횟수는 4 * 3 * 3, 한점에서 최대 36번 수행
+                check[i][j] = false;
+
+                // ㅏ 모양 검사
+                check_exshape(i, j);
+            }
+        }
+
+        System.out.println(result);
     }
 
-    private static List<Shape> makeShape() {
-        List<Shape> list = new ArrayList<>();
-
-        int[][] points = {
-            {0, 0}, {0, 1}, {0, 2}, {0, 3},
-            {0, 0}, {0, 1}, {1, 0}, {1, 1},
-            {0, 0}, {1, 0}, {2, 0}, {2, 1},
-            {0, 0}, {1, 0}, {1, 1}, {2, 1},
-            {0, 0}, {0, 1}, {0, 2}, {1, 1},
-        };
-
-        for (int i = 0; i < 5; i++) {
-            List<Point> position = new ArrayList<>();
-            for (int j = 0; j < 4; j++) {
-                int x = points[i * 4 + j][0];
-                int y = points[i * 4 + j][1];
-                position.add(new Point(x, y));
-            }
-            list.add(new Shape(position));
+    public static void dfs(int x, int y, int sum_value, int length) {
+        // 길이가 4 이상이면 종료
+        if (length >= 4) {
+            result = Math.max(result, sum_value);
+            return;
         }
 
-        return list;
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            // check
+            if (nx < 1 || nx > n || ny < 1 || ny > m) {
+                continue;
+            }
+
+            if (check[nx][ny] == false) {
+                check[nx][ny] = true;
+
+                dfs(nx, ny, sum_value + a[nx][ny], length + 1);
+
+                check[nx][ny] = false;
+            }
+        }
+    }
+
+    public static void check_exshape(int x, int y) {
+        for (int i = 0; i < 4; i++) {
+            boolean isOut = false;
+            int sum_value = 0;
+
+            for (int j = 0; j < 4; j++) {
+                int nx = x + ex[i][j];
+                int ny = y + ey[i][j];
+
+                if (nx < 1 || nx > n || ny < 1 || ny > m) {
+                    isOut = true;
+                    break;
+                } else {
+                    sum_value += a[nx][ny];
+                }   
+            }
+
+            if(!isOut) {
+                result = Math.max(result, sum_value);
+            }
+        }
     }
 }
