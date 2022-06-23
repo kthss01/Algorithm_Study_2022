0ac92e8e6d6e00b6765468e4307205ecdaeef198
kthss01 <kthss01@naver.com>
2022-06-24 08:01:43 +0900

    알고리즘 공부 63일차

--- JavaScript/leetcode/easy/prob_27/main.js
+++ JavaScript/leetcode/easy/prob_27/main.js
@@ -18,8 +18,14 @@
  * @return {number}
  */
 const removeElement = function (nums, val) {
-    nums = nums.filter((num) => num != val);
-    return nums;
+    const temp = nums.filter((num) => num != val);
+    nums.length = temp.length;
+    for (let i = 0; i < nums.length; i++) {
+        if (i < temp.length) {
+            nums[i] = temp[i];
+        }
+    }
+    return nums.length;
 };
 
 var nums;
@@ -28,12 +34,16 @@ var val;
 // ex1
 nums = [3, 2, 2, 3];
 val = 3;
+
 console.log(nums);
 console.log(removeElement(nums, val));
 console.log(nums);
 
+console.log("\n");
+
 nums = [0, 1, 2, 2, 3, 0, 4, 2];
 val = 2;
+
 console.log(nums);
 console.log(removeElement(nums, val));
 console.log(nums);
